cf7917eb961b6629047dbf9befd3c42c860e2db5
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-07-01 15:52:55 -0400

    creating branch and first steps to connect the API

--- src/store/flux.jsx
+++ src/store/flux.jsx
@@ -1,11 +1,27 @@
 const getState = ({ getStore, getActions, setStore }) => {
     return {
         store: {
-            
+            users: null,
         },
 
         actions: {
-            
+            getUsers: url => {
+                fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then(resp => resp.json())
+                    .then(data => {
+                        setStore({
+                            users: data
+                        });
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            },
         }
 
     }
