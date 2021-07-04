f2034aa33b7e4385a84d1aec9684f59001d42a28
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-07-03 20:34:14 -0400

    adding info of appcontext

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../store/appContext';
+import { Link } from 'react-router-dom';
+import { Card } from 'antd';
 
 const Home = props => {
-    return(
+    const { store, actions } = useContext(Context);
+    const { users } = store;
+    return (
         <section>
             <h1>HOME</h1>
+            <div className="row d-flex justify-content-center ml-5">
+                {
+                    !!store.users ?
+                        store.users.map((user, i) => {
+                            return (
+                                <Card title={user.name} extra={<Link to={"/users/" + user.id}>More</Link>} style={{ width: 300 }}>
+                                    <p>{user.username}</p>
+                                    <p>{user.email}</p>
+                                    <p>{user.phone}</p>
+                                </Card>
+                            )
+                        })
+                        : (
+                            <div class="text-center" id="undefined">
+                                <div className="spinner-grow text-light" role="status">
+                                </div>
+                            </div>
+                        )
+                }
+            </div>
+
         </section>
     );
 }
